9e802c33d199d9e08fb44cfff2565d72936b9f24
luyou7 <88639430+luyou7@users.noreply.github.com>
2025-06-09 17:37:31 +0800

    data

--- src/BMW_E31.cpp
+++ src/BMW_E31.cpp
@@ -1,197 +1,168 @@
 /*
- * This file is part of the Zombieverter project.
+ * 该文件属于 Zombieverter 项目。
  *
- * Copyright (C) 2023 Damien Maguire
+ * 版权所有 (C) 2023 Damien Maguire
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * 本程序是自由软件：您可以根据 GNU 通用公共许可证第 3 版或其任何后续版本，
+ * 重新分发和/或修改本程序。
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * 本程序按“希望有用”的原则发布，但不提供任何形式的担保，
+ * 包括适销性或适合特定用途的隐含担保。
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 您应当已收到 GNU 通用公共许可证副本。
+ * 若没有，请访问 <http://www.gnu.org/licenses/>。
  */
-#include "BMW_E31.h"
-#include "hwinit.h"
-#include <libopencm3/stm32/timer.h>
-#include <libopencm3/stm32/gpio.h>
+
+#include "BMW_E31.h"         // 包含 BMW_E31 类的声明
+#include "hwinit.h"          // 硬件初始化相关
+#include <libopencm3/stm32/timer.h> // STM32 定时器相关函数
+#include <libopencm3/stm32/gpio.h>  // STM32 GPIO 相关函数
+
 /*
-*E31 840CI Tacho:
-*1000RPM = 70Hz
-*2000RPM = 140Hz
-*5000RPM = 345Hz
-*6000RPM = 413Hz
-*/
+ * E31 840CI 仪表转速表参考频率：
+ * 1000 转/分 = 70 Hz
+ * 2000 转/分 = 140 Hz
+ * 5000 转/分 = 345 Hz
+ * 6000 转/分 = 413 Hz
+ */
 
-
-//We use this as an init function
+// 作为初始化函数使用
 void BMW_E31::SetCanInterface(CanHardware* c)
 {
-    can = c;
-    tim_setup();//Fire up timer one...
-    timer_disable_counter(TIM1);//...but disable until needed
-    //note we are trying to reuse the lexus gs450h oil pump pwm output here to drive the tach
-    //Be aware this will prevent combo of E31 and GS450H for now ...
-    timerIsRunning=false;
+    can = c;                   // 设置 CAN 硬件接口指针
+    tim_setup();               // 初始化定时器1
+    timer_disable_counter(TIM1); // 禁用定时器，待用时再启动
+    // 这里尝试复用 Lexus GS450h 油泵的 PWM 输出信号来驱动转速表
+    // 注意：这会导致 E31 与 GS450H 不能同时使用此功能
+    timerIsRunning = false;    // 定时器状态标记，初始未启动
 
-    can->RegisterUserMessage(0x153);//ASC message. Will confirm.
+    can->RegisterUserMessage(0x153); // 注册 CAN ID 0x153 的消息（ASC消息），待确认具体含义
 }
 
-
+// 设置转速表的转速值，单位是转/分（RPM）
 void BMW_E31::SetRevCounter(int speed)
 {
-
     uint16_t speed_input = speed;
-    speed_input = MAX(750, speed_input);//
-    speed_input = MIN(7500, speed_input);
-    timerPeriod = 30000000 / speed_input; //TODO: find correct factor or make parameter. current gives 52Hz at 750rpm.
-    timer_set_period(TIM1, timerPeriod);
-    timer_set_oc_value(TIM1, TIM_OC1, timerPeriod / 2); //always stay at 50% duty cycle
-
+    speed_input = MAX(750, speed_input);   // 限制最小转速为 750 RPM
+    speed_input = MIN(7500, speed_input);  // 限制最大转速为 7500 RPM
+    // 计算定时器周期，TODO: 需要找到准确系数或改为参数化
+    // 当前系数导致 750 RPM 时大约 52Hz
+    timerPeriod = 30000000 / speed_input;
+    timer_set_period(TIM1, timerPeriod);          // 设置定时器周期
+    timer_set_oc_value(TIM1, TIM_OC1, timerPeriod / 2); // 设置输出比较值，50% 占空比
 }
 
-
+// 设置温度表的显示（当前未实现具体逻辑）
 void BMW_E31::SetTemperatureGauge(float temp)
 {
-    float dc = temp * 10; //TODO find right factor for value like 0..0.5 or so
-    //Would like to use digi pots here
-    dc = dc;
+    float dc = temp * 10; // TODO: 需找到合适的比例系数，期望输出0~0.5区间
+    // 未来希望使用数字电位器控制
+    dc = dc; // 防止编译器警告（目前无实际功能）
 }
 
+// 解码 CAN 消息
 void BMW_E31::DecodeCAN(int id, uint32_t* data)
 {
-    uint8_t* bytes = (uint8_t*)data;//E31 CAN to be added here
+    uint8_t* bytes = (uint8_t*)data; // 将数据转换为字节数组，便于解析
 
-    if (id == 0x153)// ASC1 contains road speed signal. Unsure if applies to E31 as yet ....
+    if (id == 0x153) // 如果是 0x153 的 ASC1 消息
     {
-        //Vehicle speed signal in Km/h
-        //Calculation = ( (HEX[MSB] * 256) + HEX[LSB]) * 0.0625
-        //Min: 0x160 (0 Km/h)
-
+        // 车速信号，单位 km/h
+        // 计算方法: ((高字节 * 256) + 低字节 - 0x160) * 0.0625
+        // 最小值 0x160 表示 0 km/h
         float road_speed = 0.0625f * (((bytes[2] << 8) | (bytes[1])) - 0x160);
 
-        Param::SetFloat(Param::Veh_Speed, road_speed);
+        Param::SetFloat(Param::Veh_Speed, road_speed); // 保存车速参数
     }
 }
 
-void BMW_E31::EGSMsg43B()  //EGS1
+// 发送 EGS 0x43B 消息，常用于自动变速器控制
+void BMW_E31::EGSMsg43B()
 {
-
     uint8_t bytes[3];
 
-    bytes[0]=0x46;
-    bytes[1]=0x00;
-    bytes[2]=0x00;
+    bytes[0] = 0x46;  // 数据字节1
+    bytes[1] = 0x00;  // 数据字节2
+    bytes[2] = 0x00;  // 数据字节3
 
-    can->Send(0x43B, (uint32_t*)bytes,3);
+    can->Send(0x43B, (uint32_t*)bytes, 3); // 发送 CAN 消息 ID 0x43B，3字节数据
 }
 
+// 发送变速箱状态消息（CAN ID 0x43F）
+// gear参数代表档位，-1 反向，0 空挡，1 驱动，默认其它
 void BMW_E31::EGSMsg43F(int8_t gear)
 {
-    //Can bus data packet values to be sent
     uint8_t bytes[8];
-    // Source: https://www.bimmerforums.com/forum/showthread.php?1887229-E46-Can-bus-project&p=30055342#post30055342
-    // byte 0 = 0x81 //doesn't do anything to the ike
-    bytes[0] = 0x81;
-    // byte 1 = 0x01 where;
-    // 01 = first gear
-    // 02= second gear
-    // 03 = third gear
-    // 04 = fourth gear
-    // 05 = D
-    // 06 = N
-    // 07 = R
-    // 08 = P
-    // 09 = 5
-    // 0A = 6
+
+    // 参考来源：BimmerForums 讨论帖
+    bytes[0] = 0x81; // 固定值，不影响仪表
+
+    // 根据档位设置字节1的值
     switch (gear)
     {
-    case -1 /* Reverse */:
+    case -1: // 倒档
         bytes[1] = 0x07;
         break;
-    case 0 /* Neutral */:
+    case 0:  // 空挡
         bytes[1] = 0x06;
         break;
-    case 1 /* Drive */:
+    case 1:  // 驱动档（D）
         bytes[1] = 0x05;
         break;
-    default:
+    default: // 其它档位，显示为P档
         bytes[1] = 0x08;
         break;
     }
 
-    // byte 2 = 0xFF where;
-    // FF = no display
-    // 00 = E
-    // 39 = M
-    // 40 = S
-    bytes[2] = 0xFF;
+    bytes[2] = 0xFF; // 不显示任何特殊符号
+    bytes[3] = 0xFF; // 不影响仪表
+    bytes[4] = 0x00; // 不影响仪表
+    bytes[5] = 0x80; // 清除档位警告灯
+    bytes[6] = 0xFF; // 不影响仪表
+    bytes[7] = 0xFF; // 不影响仪表
 
-    // byte 3 = 0xFF //doesn't do anything to the ike
-    bytes[3] = 0xFF;
-
-    // byte 4 = 0x00 //doesn't do anything to the ike
-    bytes[4] = 0x00;
-
-    // byte 5 = 0x80 where;
-    // 80 = clears the gear warning picture - all other values bring it on
-    bytes[5] = 0x80;
-
-    // byte 6 = 0xFF //doesn't do anything to the ike
-    bytes[6] = 0xFF;
-
-    // byte 7 = 0x00 //doesn't do anything to the ike
-    bytes[7] = 0xFF;
-
-    can->Send(0x43F, bytes, 8);
+    can->Send(0x43F, bytes, 8); // 发送变速器状态消息
 }
 
+// 1毫秒任务（当前未实现）
 void BMW_E31::Task1Ms()
 {
 
-
 }
 
-
+// 10毫秒任务，检测点火开关状态并发送相关消息
 void BMW_E31::Task10Ms()
 {
-    if(DigIo::t15_digi.Get() == 1)
+    if(DigIo::t15_digi.Get() == 1) // 点火开关接通
     {
-        EGSMsg43B();//EGS1
-        if (Param::GetBool(Param::Transmission))
-            EGSMsg43F(Param::GetInt(Param::dir));
+        EGSMsg43B(); // 发送变速箱消息43B
+        if (Param::GetBool(Param::Transmission)) // 如果变速箱使能
+            EGSMsg43F(Param::GetInt(Param::dir)); // 发送当前档位消息43F
     }
-
 }
 
-
+// 100毫秒任务，控制定时器启动和关闭
 void BMW_E31::Task100Ms()
 {
-
     if(Param::GetInt(Param::T15Stat) && !timerIsRunning)
     {
-        timer_enable_counter(TIM1);
-        timerIsRunning=true;
+        timer_enable_counter(TIM1); // 点火接通且定时器未启动，启动定时器
+        timerIsRunning = true;
     }
     else if (!Param::GetInt(Param::T15Stat) && timerIsRunning)
     {
-        timer_disable_counter(TIM1);
-        timerIsRunning=false;
+        timer_disable_counter(TIM1); // 点火断开且定时器正在运行，关闭定时器
+        timerIsRunning = false;
     }
-
 }
 
-
+// 返回是否点火开关已接通
 bool BMW_E31::Ready()
 {
     return DigIo::t15_digi.Get();
 }
 
+// 返回是否发动机启动信号为真
 bool BMW_E31::Start()
 {
     return Param::GetBool(Param::din_start);
--- src/Can_OI.cpp
+++ src/Can_OI.cpp
@@ -2,22 +2,16 @@
  *
  * Copyright (C) 2020 Johannes Huebner <dev@johanneshuebner.com>
  *                      Damien Maguire <info@evbmw.com>
- * Yes I'm really writing software now........run.....run away.......
+ * 这里是版权信息和作者联系方式
+ * “是的，我现在真的在写软件了……快跑……快跑吧……”
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * 该程序是自由软件：您可以根据GNU通用公共许可证（GPL）第3版或（您选择的）更高版本自由地重新发布和/或修改本程序。
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * 该程序发布的目的是希望它有用，但不保证任何形式的担保，包括对适销性或适用于特定目的的隐含保证。
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 您应当已收到一份GNU通用公共许可证的副本。如果没有，请访问 http://www.gnu.org/licenses/ 。
  *
- *New implementation as of V2.02. See : https://openinverter.org/wiki/CAN_communication
+ * 本文件基于V2.02版本重新实现。更多信息见：https://openinverter.org/wiki/CAN_communication
  */
 
 #include "Can_OI.h"
@@ -26,127 +20,145 @@
 #include "stm32_can.h"
 #include "params.h"
 
-uint8_t Can_OI::run100ms = 0;
-uint32_t Can_OI::lastRecv = 0;
-uint16_t Can_OI::voltage;
-int16_t Can_OI::speed;
-bool Can_OI::error=false;
-int16_t Can_OI::inv_temp;
-int16_t Can_OI::motor_temp;
-int16_t Can_OI::final_torque_request;
-static bool statusInv = 0;
-uint8_t Inv_Opmode=0;
-int opmode;
-uint16_t InvStartTimeout=0;
+// 静态变量定义
+uint8_t Can_OI::run100ms = 0;       // 100ms定时任务标志
+uint32_t Can_OI::lastRecv = 0;      // 最近一次CAN接收时间戳
+uint16_t Can_OI::voltage;            // 逆变器电压
+int16_t Can_OI::speed;               // 电机转速
+bool Can_OI::error=false;            // 错误状态标志
+int16_t Can_OI::inv_temp;            // 逆变器温度
+int16_t Can_OI::motor_temp;          // 电机温度
+int16_t Can_OI::final_torque_request; // 最终扭矩请求值
+static bool statusInv = 0;           // 逆变器状态标志，静态局部变量
+uint8_t Inv_Opmode=0;                // 逆变器工作模式
+int opmode;                         // 当前操作模式
+uint16_t InvStartTimeout=0;          // 逆变器启动超时计数器
 
+// 设置CAN接口并注册关心的消息ID
 void Can_OI::SetCanInterface(CanHardware* c)
 {
    can = c;
 
-   can->RegisterUserMessage(0x190);//Open Inv Msg. Dec 400 for RPM.
-   can->RegisterUserMessage(0x19A);//Open Inv Msg. Dec 410 for temps
-   can->RegisterUserMessage(0x1A4);//Open Inv Msg. Dec 420 for Voltage.
-   can->RegisterUserMessage(0x1AE);//Open Inv Msg. Dec 430 for Opmode.
+   // 注册不同的消息ID，这些消息包含电机转速、电压、温度和工作模式
+   can->RegisterUserMessage(0x190); // 电机转速消息，ID为0x190，解码位400
+   can->RegisterUserMessage(0x19A); // 温度消息，ID为0x19A，解码位410
+   can->RegisterUserMessage(0x1A4); // 电压消息，ID为0x1A4，解码位420
+   can->RegisterUserMessage(0x1AE); // 工作模式消息，ID为0x1AE，解码位430
 }
 
+// 解码接收到的CAN消息，根据ID解析不同数据
 void Can_OI::DecodeCAN(int id, uint32_t data[2])
 {
-//0x1A4 bits 0-15 inverter voltage x10
-//0x190 bits 0-15 motor rpm x1
-//0x19A bits 0-15 heatsink temp x10
+   // CAN消息格式说明：
+   // 0x1A4消息中，位0-15为逆变器电压*10
+   // 0x190消息中，位0-15为电机转速
+   // 0x19A消息中，位0-15为散热器温度*10
 
-   uint8_t* bytes = (uint8_t*)data;// arrgghhh this converts the two 32bit array into bytes. See comments are useful:)
+   uint8_t* bytes = (uint8_t*)data; // 将32位数据转换为字节数组便于处理
 
-   if (id == 0x1A4)// THIS MSG CONTAINS INV VOLTAGE
+   if (id == 0x1A4) // 逆变器电压消息处理
    {
-      voltage=((bytes[1]<<8)|(bytes[0]))/10;
+      // 电压是低字节在前，高字节在后，转换为实际电压值
+      voltage = ((bytes[1] << 8) | (bytes[0])) / 10;
    }
-   else if (id == 0x190)// THIS MSG CONTAINS MOTOR RPM
+   else if (id == 0x190) // 电机转速消息处理
    {
-      speed=((bytes[1]<<8)|(bytes[0]));
+      speed = ((bytes[1] << 8) | (bytes[0]));
    }
-   else if (id == 0x19A)// THIS MSG CONTAINS INVERTER HEATSINK TEMP
+   else if (id == 0x19A) // 逆变器散热器温度消息处理
    {
-      inv_temp = ((bytes[1]<<8)|(bytes[0]))/10;//INVERTER TEMP
-      motor_temp = 0;//MOTOR TEMP
+      inv_temp = ((bytes[1] << 8) | (bytes[0])) / 10; // 逆变器温度
+      motor_temp = 0; // 电机温度暂时未使用，设为0
    }
-
-      else if (id == 0x1AE)// THIS MSG CONTAINS OPMODE
+   else if (id == 0x1AE) // 逆变器工作模式消息处理
    {
-      Inv_Opmode = bytes[0];//INVERTER OPMODE
-      //0=Off, 1=Run, 2=ManualRun, 3=Boost, 4=Buck, 5=Sine, 6=AcHeat
-
+      Inv_Opmode = bytes[0]; // 工作模式字节
+      // 具体模式说明：
+      // 0=关闭, 1=运行, 2=手动运行, 3=Boost模式, 4=Buck模式, 5=正弦波, 6=交流加热
    }
 }
 
+// 设置请求的扭矩百分比
 void Can_OI::SetTorque(float torquePercent)
 {
+   // 将扭矩百分比转换成整型值（放大10倍），以便发送
    final_torque_request = torquePercent * 10;
-   Param::SetInt(Param::torque,final_torque_request);//post processed final torue value sent to inv to web interface
+
+   // 设置参数系统中的扭矩值，供web接口等使用
+   Param::SetInt(Param::torque, final_torque_request);
+
+   // 读取当前操作模式
    int opmode = Param::GetInt(Param::opmode);
-   uint8_t tempIO=0;
 
-   if(Param::GetBool(Param::din_forward) && opmode==MOD_RUN) tempIO+=8;//only send direction data if in run mode
-   if(Param::GetBool(Param::din_reverse) && opmode==MOD_RUN) tempIO+=16;//only send direction data if in run mode
-   if(Param::GetBool(Param::din_brake)) tempIO+=4;
-   //if(Param::GetBool(Param::din_start)) tempIO+=2;
-   if(opmode==MOD_RUN && InvStartTimeout!=0)//Set the start signal true for 3 seconds on vcu entering run mode.
-    {
-      InvStartTimeout--;
-      tempIO+=2;//inv start signal on for 3 secs once enter vcu run mode
-    }
+   uint8_t tempIO = 0; // 用于存放方向和状态的IO位
 
-   if(opmode==MOD_OFF)
+   // 只有在运行模式下，才发送前进和倒退方向信息
+   if (Param::GetBool(Param::din_forward) && opmode == MOD_RUN) tempIO += 8;
+   if (Param::GetBool(Param::din_reverse) && opmode == MOD_RUN) tempIO += 16;
+   if (Param::GetBool(Param::din_brake)) tempIO += 4;
+   // if(Param::GetBool(Param::din_start)) tempIO+=2; // 启动信号，暂时注释
+
+   // 进入运行模式时，启动信号保持3秒
+   if (opmode == MOD_RUN && InvStartTimeout != 0)
    {
-     InvStartTimeout=300;
+      InvStartTimeout--;
+      tempIO += 2; // 启动信号有效
    }
 
-   uint32_t data[2];
-   uint32_t pot = Param::GetInt(Param::pot) & 0xFFF;
-   uint32_t pot2 = Param::GetInt(Param::pot2) & 0xFFF;
-   uint32_t canio = tempIO & 0x3F;
-   uint32_t ctr = Param::GetInt(Param::canctr) & 0x3;
-   uint32_t cruise = Param::GetInt(Param::cruisespeed) & 0x3FFF;
-   uint32_t regen = 0x00;//Param::GetInt(Param::potbrake) & 0x7F;
+   // 退出运行模式，重置启动超时计数
+   if (opmode == MOD_OFF)
+   {
+      InvStartTimeout = 300;
+   }
 
+   // 构造CAN数据包内容
+   uint32_t data[2];
+   uint32_t pot = Param::GetInt(Param::pot) & 0xFFF;       // 油门信号，占12位
+   uint32_t pot2 = Param::GetInt(Param::pot2) & 0xFFF;     // 第二油门信号，占12位
+   uint32_t canio = tempIO & 0x3F;                         // IO信号占6位
+   uint32_t ctr = Param::GetInt(Param::canctr) & 0x3;      // 计数器，占2位
+   uint32_t cruise = Param::GetInt(Param::cruisespeed) & 0x3FFF; // 巡航速度，占14位
+   uint32_t regen = 0x00;                                  // 再生制动，目前为0
+
+   // 组合数据位，符合CAN协议要求
    data[0] = pot | (pot2 << 12) | (canio << 24) | (ctr << 30);
    data[1] = cruise | (ctr << 14) | (regen << 16);
 
+   // 计算CRC校验码，并放入数据末尾
    crc_reset();
    uint32_t crc = crc_calculate_block(data, 2) & 0xFF;
    data[1] |= crc << 24;
 
-   can->Send(0x3F,data);//send 0x3F
+   // 发送CAN消息，ID为0x3F
+   can->Send(0x3F, data);
 }
 
-
+// 获取逆变器当前状态，1为运行，0为关闭
 int Can_OI::GetInverterState()
 {
-   if(Inv_Opmode==0) statusInv = 0;
-   if(Inv_Opmode==1) statusInv = 1;
-   if(opmode==MOD_OFF) statusInv = 0;//Resort to off if vcu is in off state.
+   if (Inv_Opmode == 0) statusInv = 0;
+   if (Inv_Opmode == 1) statusInv = 1;
+
+   // 如果操作模式为关闭，强制状态为关闭
+   if (opmode == MOD_OFF) statusInv = 0;
+
    return statusInv;
 }
 
-
-
-
+// 100ms定时任务，定时更新和清理变量
 void Can_OI::Task100Ms()
 {
-opmode = Param::GetInt(Param::opmode);
-if(opmode==MOD_OFF)
-{
-  voltage=0;//Clear all vals when in off mode to ensure real vals are displayed on next start
-  speed=0;
-  inv_temp=0;
-  motor_temp=0;
-  Inv_Opmode=0;
-  final_torque_request=0;
+   // 读取当前操作模式
+   opmode = Param::GetInt(Param::opmode);
+
+   if (opmode == MOD_OFF)
+   {
+      // 当处于关闭状态时，清零所有参数，确保下次启动时显示真实值
+      voltage = 0;
+      speed = 0;
+      inv_temp = 0;
+      motor_temp = 0;
+      Inv_Opmode = 0;
+      final_torque_request = 0;
+   }
 }
-
-}
-
-
-
-
-
